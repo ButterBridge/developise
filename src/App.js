b10809c0ca69a88ba849e13a62a2aa71ed577e90
Jonny Rathbone <jonnyrathbone@gmail.com>
2018-07-18 23:28:03 +0100

    load player on componenDidMount

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import { Person } from './lib/player';
 class App extends Component {
     state = {
         day : 0,
-        player : new Person('Jonty')
+        player : null
+    }
+
+    componentDidMount = () => {
+        this.setState({
+            player : new Person('Jonty')
+        })
     }
 
     render() {
         const {player} = this.state;
+        if (!player) return null;
         return (
             <div className="App">
                 Hi {player.name}!
