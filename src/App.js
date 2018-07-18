49215bef221d93fb36ea7d0c7c7d4aac7e31adbc
Jonny Rathbone <jonnyrathbone@gmail.com>
2018-07-18 23:30:43 +0100

    progress the day with button

--- src/App.js
+++ src/App.js
@@ -15,16 +15,21 @@ class App extends Component {
     }
 
     render() {
-        const {player} = this.state;
+        const {player, day} = this.state;
         if (!player) return null;
         return (
             <div className="App">
-                Hi {player.name}!
+                <h1>Hi {player.name}! It's day {day}.</h1>
+                <button onClick={this.progress}>The next day...</button>
             </div>
         );
     }
 
-    
+    progress = () => {
+        this.setState({
+            day : this.state.day + 1
+        })
+    }
 }
 
 export default App;
